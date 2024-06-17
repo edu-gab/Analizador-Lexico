540712cb93d74139e2bff7db353262aa2b9a89dc
Eduardo <egsanche@espol.edu.ec>
2024-06-17 16:12:13 -0500

    Update

--- analizador.py
+++ analizador.py
@@ -1,2 +1,50 @@
 import ply.lex as lex
 
+tokens = (
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'MODULE',
+    'DIVIDE',
+    'AND',
+    'OR',
+    'NEGATE'
+)
+
+t_PLUS = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_MODULE = r'\%'
+t_DIVIDE = r'/'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+    
+t_ignore = ' \t'
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+    
+lexer = lex.lex()
+
+data = '''
+3 + 4 * 10 % &
+'''
+
+lexer.input(data)
+
+while True:
+    tok = lexer.token()
+    
+    if not tok:
+        break
+    
+    print(tok)
