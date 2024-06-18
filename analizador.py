1333bd10f0cd6f38f9f8dfde15ade186beac2b8a
Robespierre <99456899+robtrivi@users.noreply.github.com>
2024-06-17 23:06:33 -0500

    Se automatiza el log y la lectura de cada estudiante por usuario en github

--- analizador.py
+++ analizador.py
@@ -1,50 +1,54 @@
 import ply.lex as lex
 import datetime
 
-#Comienza aporte Robespierre Triviño Roman
+estudiante = "robtrivi"
+
+
+# Comienza aporte Robespierre Triviño Roman
 def generar_nombre_log(nombre):
     ahora = datetime.datetime.now()
     nombre_archivo = ahora.strftime(f"lexico-{nombre}-%d%m%Y-%Hh%M.txt")
     return nombre_archivo
 
+
 def leer_algoritmo(nombre_archivo):
     try:
-        with open("algoritmos/"+nombre_archivo, 'r') as archivo:
+        with open("algoritmos/prueba_" + nombre_archivo, 'r') as archivo:
             contenido = archivo.read()
         return contenido
     except FileNotFoundError:
         print(f"El archivo {nombre_archivo} no existe.")
         return ""
 
-#Termina aporte Robespierre Triviño Roman
 
+# Termina aporte Robespierre Triviño Roman
 
 '''
 Eduardo Sanchez
 '''
 tokens = (
     'NUMBER', 'FLOAT', 'BOOLEAN', 'NULL', 'CHAR', 'STRING',
-    'PLUS', 'MINUS', 'TIMES', 'MODULE','DIVIDE', 'MOD',
+    'PLUS', 'MINUS', 'TIMES', 'MODULE', 'DIVIDE', 'MOD',
     'INCREMENT', 'DECREMENT', 'AND', 'OR', 'NOT',
     'ASSIGN', 'ADDEQ', 'SUBEQ', 'MULTEQ', 'DIVEQ', 'MODEQ',
     'EQEQ', 'NOTEQ', 'LTEQ', 'GTEQ', 'LT', 'GT',
     'LPAREN', 'RPAREN', 'LBRACE', 'RBRACE', 'LBRACK', 'RBRACK',
-    'COMMA', 'RANGE','DOT', 'COLON', 'SEMICOLON', 'ARROW', 'DOUBLECOLON',
+    'COMMA', 'RANGE', 'DOT', 'COLON', 'SEMICOLON', 'ARROW', 'DOUBLECOLON',
     'ID', 'COMMENT', 'COMMENT_MULTI'
 )
 
 t_PLUS = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
+t_MINUS = r'-'
+t_TIMES = r'\*'
 t_MODULE = r'\%'
 t_DIVIDE = r'/'
-t_MOD = r'%' #Robespierre Triviño
+t_MOD = r'%'  # Robespierre Triviño
 t_INCREMENT = r'\+\+'
 t_DECREMENT = r'\-\-'
 t_AND = r'&&'
 t_OR = r'\|\|'
 t_NOT = r'!'
-t_ASSIGN = r'\='#Robespierre Triviño
+t_ASSIGN = r'\='  # Robespierre Triviño
 # Robespierre Triviño
 t_ADDEQ = r'\+='
 t_SUBEQ = r'-='
@@ -79,61 +83,86 @@ def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
+
+
 # Aqui comienza aporte de Robespierre
 def t_FLOAT(t):
     r'\d+\.\d+([eE][+-]?\d+)?'
     t.value = float(t.value)
     return t
 
+
 def t_BOOLEAN(t):
     r'true|false'
     t.value = (t.value == 'true')
     return t
+
 
 def t_NULL(t):
     r'null'
     t.value = None
     return t
 
+
 def t_CHAR(t):
     r'\'([^\\\n]|(\\[nrt0\'"\\]))\''
     t.value = t.value[1:-1]
     return t
+
 
 def t_STRING(t):
     r'\"([^\\\n]|(\\[nrt0\'"\\]))*\"'
     t.value = t.value[1:-1]
     return t
 
+
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     return t
+
 
 def t_COMMENT(t):
     r'//.*'
     pass
 
+
 def t_COMMENT_MULTI(t):
     r'/\*(.|\n)*?\*/'
     pass
+
+
 # Aqui termina aporte de Robespierre
+'''
+Comienza aporte Eduardo Sanchez
+'''
+
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+
 t_ignore = ' \t'
+
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+
 lexer = lex.lex()
 
+'''
+Termina aporte Eduardo Sanchez
+'''
 # Comienza aporte Robespierre Triviño
-data = leer_algoritmo("prueba_robtrivi.txt")
-file = open("logs/"+generar_nombre_log("robtrivi"), "w")
+data = leer_algoritmo(estudiante + ".txt")
+file = open("logs/" + generar_nombre_log(estudiante), "w")
 # Termina aporte Robespierre Triviño
+
+'''
+Comienza aporte Eduardo Sanchez
+'''
 lexer.input(data)
 while True:
     tok = lexer.token()
@@ -144,5 +173,5 @@ while True:
 
 file.close()
 '''
-Eduardo Sanchez
+Termina aporte Eduardo Sanchez
 '''
