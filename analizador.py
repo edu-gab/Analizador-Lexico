65f9821db369f7cbaab76f7e856d7b8235d1dc45
Robespierre <99456899+robtrivi@users.noreply.github.com>
2024-06-24 22:38:03 -0500

    Se modifica orden de archivos

--- analizador.py
+++ analizador.py
@@ -112,8 +112,6 @@ def t_NUMBER(t):
     t.value = int(t.value)
     return t
 
-
-
 def t_BOOLEAN(t):
     r'true|false'
     t.value = (t.value == 'true')
@@ -185,73 +183,3 @@ data = leer_algoritmo(estudiante + ".txt")
 file = open("logs/" + generar_nombre_log(estudiante), "w")
 # Termina aporte Robespierre Triviño
 
-
-## Analizador Sintáctico
-
-# Comienza aporte de Ronny
-
-# Estructura de control (WHILE)
-def p_while(p):
-  'ejec : WHILE LPAREN ID comparation INT RPAREN LBRACE option RBRACE'
-
-# Estructura de datos (CLASS)
-def p_classD(p):
-  'ejec : CLASS ID LBRACE atribC RBRACE'
-
-
-def p_atrib(p):
-  'atrib : vas ID DOUBLEP TYPEDATA EQUALS data'
-
-
-def p_comparation(p):
-  '''comparation : EQUALS
-  | GREATERTHAN
-  | LESSTHAN
-  '''
-
-
-def p_print(p):
- 'print : PRINTLN LPAREN data RPAREN'
-
-def p_printDW(p):
-  '''printDW : print 
-  | print printDW
-  '''
-#declaracion de funcion
-def p_Sfunction(p):
-  'ejec : FUN ID LPAREN funcionparametro RPAREN DOUBLEP data LBRACE RETURN ID RBRACE'
-
-
-def p_dataT(p):
-  'data : INT | CHAR | FLOAT | STR '
-
-def p_atribC(p):
-  ''' atribC : atrib
-  | atrib atribC
-  '''
-def p_option(p):
-  '''option : assignment
-  | assignmentL 
-  | printDW
-  '''
-def p_Cif(p): 
-  'ejec : IF LPAREN ID comparation INT RPAREN LBRACE option RBRACE'
-
-def p_elif(p):
-  'ejec : IF LPAREN ID comparation INT RPAREN LBRACE option RBRACE ELSE LBRACE option RBRACE' 
-  
-  
-def p_assignment(p):
-  'assignment : vas ID EQUALS data'
-
-
-def p_assignmentL(p):
-  'assignmentL : assignmentL assignment'
-
-
-def p_assignmentLU(p):
-  'assignmentL : assignment'
-
-
-def p_vas(p):
-  'vas : VAL|VAR'
