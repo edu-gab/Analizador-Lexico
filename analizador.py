31ac7d8cc1a1f98dc6322ab3eba20b39b79369e0
Ronny Garcia <rsgarcia@espol.edu.ec>
2024-06-24 22:00:45 -0500

    Refactor analizador.py for improved code structure and readability

--- analizador.py
+++ analizador.py
@@ -180,18 +180,73 @@ data = leer_algoritmo(estudiante + ".txt")
 file = open("logs/" + generar_nombre_log(estudiante), "w")
 # Termina aporte Robespierre Triviño
 
-'''
-Comienza aporte Eduardo Sanchez
-'''
-lexer.input(data)
-while True:
-    tok = lexer.token()
-    if not tok:
-        break
-    file.write(f"{tok}\n")
-    print(tok)
 
-file.close()
-'''
-Termina aporte Eduardo Sanchez
-'''
+## Analizador Sintáctico
+
+# Comienza aporte de Ronny
+#estructura dd control
+def p_while(p):
+  'ejec : WHILE LPAREN ID comparation INT RPAREN LBRACE option RBRACE'
+
+#estructura de datos
+def p_classD(p):
+  'ejec : CLASS ID LBRACE atribC RBRACE'
+
+
+def p_atrib(p):
+  'atrib : vas ID DOUBLEP TYPEDATA EQUALS data'
+
+
+def p_comparation(p):
+  '''comparation : EQUALS
+  | GREATERTHAN
+  | LESSTHAN
+  '''
+
+
+def p_print(p):
+ 'print : PRINTLN LPAREN data RPAREN'
+
+def p_printDW(p):
+  '''printDW : print 
+  | print printDW
+  '''
+#declaracion de funcion
+def p_Sfunction(p):
+  'ejec : FUN ID LPAREN funcionparametro RPAREN DOUBLEP data LBRACE RETURN ID RBRACE'
+
+
+def p_dataT(p):
+  'data : INT | CHAR | FLOAT | STR '
+
+def p_atribC(p):
+  ''' atribC : atrib
+  | atrib atribC
+  '''
+def p_option(p):
+  '''option : assignment
+  | assignmentL 
+  | printDW
+  '''
+def p_Cif(p): 
+  'ejec : IF LPAREN ID comparation INT RPAREN LBRACE option RBRACE'
+
+def p_elif(p):
+  'ejec : IF LPAREN ID comparation INT RPAREN LBRACE option RBRACE ELSE LBRACE option RBRACE' 
+  
+  
+  
+def p_assignment(p):
+  'assignment : vas ID EQUALS data'
+
+
+def p_assignmentL(p):
+  'assignmentL : assignmentL assignment'
+
+
+def p_assignmentLU(p):
+  'assignmentL : assignment'
+
+
+def p_vas(p):
+  'vas : VAL|VAR'
